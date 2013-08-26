cccefe02a2eba31c7d97a5656b7f5feb8efa6adb
Jaaziel <jaaziellopes@hotmail.com>
2013-08-26 11:11:37 -0300

    Commit inicial

---
+++ App/src/com/betha/beans/InicioBean.java
@@ -0,0 +1,17 @@
+package com.betha.beans;
+
+public class InicioBean {
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void enviar(){
+		
+	}
+}
